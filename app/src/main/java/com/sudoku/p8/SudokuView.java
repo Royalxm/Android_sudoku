468bb099d614d4219315b8b596ffd5e9b23e8acd
ROD <capone973@gmail.com>
2015-11-09 19:03:40 +0100

    first commit

---
+++ app/src/main/java/com/sudoku/p8/SudokuView.java
@@ -0,0 +1,47 @@
+package com.sudoku.p8;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+import android.view.View;
+
+/**
+ * Created by cap-one on 09/11/15.
+ */
+public class SudokuView extends SurfaceView {
+
+    private Paint linePaint;
+
+    private int width, height, cellWidth, cellHeight;
+
+    public SudokuView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        width = getWidth();
+        height = getHeight();
+        cellWidth = cellHeight = 20;
+
+        linePaint = new Paint();
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // draw vertical lines
+        for (int c = 0; c <= 9; c++) {
+            float x = (c * cellWidth);
+            canvas.drawLine(x, 0, x, height, linePaint);
+        }
+
+        // draw horizontal lines
+        for (int r = 0; r <= 9; r++) {
+            float y = r * cellHeight;
+            canvas.drawLine(0, y, width, y, linePaint);
+        }
+
+    }
+}
