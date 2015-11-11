76fc8264700e96432a0df6d9471ea8cba7bc8e9c
ROD <capone973@gmail.com>
2015-11-11 14:05:48 +0100

    Intent added, action  sur bouton Jouer

--- app/src/main/java/com/sudoku/p8/MenuActivity.java
+++ app/src/main/java/com/sudoku/p8/MenuActivity.java
@@ -4,17 +4,26 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends Activity {
+
+    Button jouer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-    }
 
-    private void startGame() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        jouer = (Button) findViewById(R.id.buttonJouer);
+
+        jouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
