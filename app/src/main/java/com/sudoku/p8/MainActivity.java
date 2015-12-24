aed20a03635b52a3eb0ba5da0de538b8731c7b09
ROD <capone973@gmail.com>
2015-12-24 13:02:55 +0100

    Changement design menu 2

--- app/src/main/java/com/sudoku/p8/MainActivity.java
+++ app/src/main/java/com/sudoku/p8/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         chrono = new Chronometer(this);
         chrono.setVisibility(View.GONE);
         actionBar.setCustomView(chrono);
+        actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowCustomEnabled(true);
 
        if(!resumeGame) chrono.setBase(SystemClock.elapsedRealtime());
